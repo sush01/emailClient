b2caf64818c7e91272e9265963e5746d75d1a953
Sushila Kandel <kandel.sushila242@gmail.com>
2025-03-25 22:51:38 +0200

    add username and password validators

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -11,9 +11,22 @@ import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 export class SignupComponent {
 
   authForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-    passwordConfirmation: new FormControl('')
+    username: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(20),
+      Validators.pattern(/[a-z0-9]+$/)
+    ]),
+    password: new FormControl('',[
+      Validators.required,
+      Validators.minLength(4),
+      Validators.maxLength(20)
+    ]),
+    passwordConfirmation: new FormControl('',[
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(20)
+    ])
   });
 
 }
