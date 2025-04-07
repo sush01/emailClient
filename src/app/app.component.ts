79c26ce5f73ed1abe91fcd2eb934bbbc2da8b29b
Sushila Kandel <kandel.sushila242@gmail.com>
2025-04-07 13:27:07 +0300

    implement asyn pipe in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { AuthService } from './auth/auth.service';
 import { CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   //title = 'emailClient';
-  signedin = false;
+  signedin$: BehaviorSubject<boolean>;
+
   constructor(private authService: AuthService){
+    this.signedin$ = this.authService.signedin$;
 
   }
-  ngOnInit(){
-    this.authService.signedin$.subscribe((signedin => {
-      this.signedin =signedin;
-    }))
-  }
+
 }
