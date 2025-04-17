c9b7a25a4a051221574237d727a2a366483bb9ec
Sushila Kandel <kandel.sushila242@gmail.com>
2025-04-17 18:59:41 +0300

    implement guard for restricting lazyloading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { RouterModule,Routes } from '@angular/router';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignoutComponent } from './auth/signout/signout.component';
-
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   { path: 'signout', component: SignoutComponent},
   { path:'signup', component: SignupComponent},
   { path:'', component: SigninComponent},
   { path:'inbox', 
+    canMatch: [authGuard],
     loadChildren:() =>
     import('./inbox/inbox.module').then((mod) => mod.InboxModule),}
   
