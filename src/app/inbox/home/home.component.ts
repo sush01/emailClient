19ce08f4874956a9a5b2f8ac0111dc7a16431693
Sushila Kandel <kandel.sushila242@gmail.com>
2025-04-30 18:59:10 +0300

    create placeholder component in the inbox setion and implement child route navigation

--- src/app/inbox/home/home.component.ts
+++ src/app/inbox/home/home.component.ts
@@ -1,8 +1,9 @@
+import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 import { EmailIndexComponent } from '../email-index/email-index.component';
 @Component({
   selector: 'app-home',
-  imports: [EmailIndexComponent],
+  imports: [EmailIndexComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- src/app/inbox/inbox-routing.module.ts
+++ src/app/inbox/inbox-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PlaceholderComponent } from './placeholder/placeholder.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent,
+    children: [
+      { path: '', component: PlaceholderComponent }
+    ]
+  }
 ];
 
 @NgModule({
