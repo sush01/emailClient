e4cf3fbad6864fcd49bc06eeb02a9b30d6ad3fe9
Sushila Kandel <kandel.sushila242@gmail.com>
2025-05-06 19:42:13 +0300

    email show component works

--- src/app/inbox/home/home.component.ts
+++ src/app/inbox/home/home.component.ts
@@ -1,6 +1,7 @@
 import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 import { EmailIndexComponent } from '../email-index/email-index.component';
+import { EmailShowComponent } from '../email-show/email-show.component';
 @Component({
   selector: 'app-home',
   imports: [EmailIndexComponent, RouterModule],
