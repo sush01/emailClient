a92b8126b0ab5cbd6f2066cbcd373d01e33d0f16
Sushila Kandel <kandel.sushila242@gmail.com>
2025-05-07 12:54:25 +0300

    implement data fetching and error handling

--- src/app/inbox/email-resolver.service.ts
+++ src/app/inbox/email-resolver.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Resolve, ActivatedRouteSnapshot, Router} from '@angular/router';
+import {EMPTY} from 'rxjs';
 import { Email } from './email';
+import { catchError } from 'rxjs';
 import { EmailService } from './email.service';
 
 @Injectable({
@@ -8,11 +10,18 @@ import { EmailService } from './email.service';
 })
 export class EmailResolverService implements Resolve<Email> {
 
-  constructor(private emailService: EmailService) { }
+  constructor(private emailService: EmailService, private router: Router) { }
 
   resolve(route: ActivatedRouteSnapshot){
     const {id} = route.params;
 
-    return this.emailService.getEmail(id);
+    return this.emailService.getEmail(id).pipe(
+      catchError(() => {
+        this.router.navigateByUrl('/inbox/not-found');
+        return EMPTY;
+
+      })
+
+    );
   }
 }
