4b97ec64baf8e343067dd93d782c36bdec8ba41e
Sushila Kandel <kandel.sushila242@gmail.com>
2025-05-07 12:30:43 +0300

    implement resolver to communicate with the data

--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -1,7 +1,5 @@
 import { Component , OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap} from 'rxjs/operators';
-import { EmailService } from '../email.service';
 import { Email } from '../email';
 import { CommonModule } from '@angular/common';
 @Component({
@@ -14,22 +12,14 @@ import { CommonModule } from '@angular/common';
 export class EmailShowComponent implements OnInit{
   email: Email | undefined;
 
-  constructor(
-    private route: ActivatedRoute,
-    private emailService: EmailService
-  ){}
-
-  ngOnInit() {
-
-    this.route.params
-    .pipe(
-      switchMap(({ id }) => {
-        return this.emailService.getEmail(id);
-      })
-    )
-    .subscribe(email => {
+  constructor(private route: ActivatedRoute,){
+    this.email = route.snapshot.data['email'];
+    this.route.data.subscribe(({ email }) => {
       this.email = email;
     });
+  }
+
+  ngOnInit() {
 
   }
 
