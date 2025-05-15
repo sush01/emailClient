23c94696e7cfe0c02f5c1200b0be8c89fcc38fd7
Sushila Kandel <kandel.sushila242@gmail.com>
2025-05-15 13:00:31 +0300

    implement sendinf reply feature

--- src/app/inbox/email-reply/email-reply.component.ts
+++ src/app/inbox/email-reply/email-reply.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { Component , Input, OnInit} from '@angular/core';
+import { ModalComponent } from '../../shared/modal/modal.component';
+import { CommonModule } from '@angular/common';
+import { Email } from '../email';
+import { EmailFormComponent } from "../email-form/email-form.component";
+import { EmailService } from '../email.service';
 
 @Component({
   selector: 'app-email-reply',
-  imports: [],
+  imports: [ModalComponent, CommonModule, EmailFormComponent],
   templateUrl: './email-reply.component.html',
   styleUrl: './email-reply.component.css'
 })
-export class EmailReplyComponent {
+export class EmailReplyComponent implements OnInit {
+showModal = false;
+@Input() email!: Email;
+text = this.email.text.replace(/\n/gi,'\n>');
 
+constructor(private emailService: EmailService){
+
+}
+
+ngOnInit() {
+    this.email = {
+      ...this.email,
+      from: this.email.to,
+      to: this.email.from,
+      subject: `RE: ${this.email.subject}`,
+      text: `\n\n\n-------------${this.email.from} wrote: \n>${this.text}`
+
+    }
+}
+
+onSubmitEmail(email: Email){
+  this.emailService.sendEmail(email).subscribe(()=> {
+    this.showModal = false;
+  })
+
+}
 }
--- src/app/inbox/email-show/email-show.component.ts
+++ src/app/inbox/email-show/email-show.component.ts
@@ -2,10 +2,11 @@ import { Component , OnInit} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Email } from '../email';
 import { CommonModule } from '@angular/common';
+import { EmailReplyComponent } from '../email-reply/email-reply.component';
 @Component({
   selector: 'app-email-show',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, EmailReplyComponent],
   templateUrl: './email-show.component.html',
   styleUrl: './email-show.component.css'
 })
